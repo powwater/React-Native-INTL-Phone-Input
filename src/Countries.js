b62159af5ec5dbfdd7e81829be008be0a0a4f9d6
Patrick Howard <33097210+phoward38@users.noreply.github.com>
2021-07-12 13:09:44 -0400

    Update KE mask to 9 digits

--- src/Countries.js
+++ src/Countries.js
@@ -107,7 +107,7 @@ const Countries = [
   { ru:"Джерси",lt:"Džersis",tr:"Jersey", en: 'Jersey',flag: '🇯🇪',code: 'JE', dialCode: '+44', mask: '99999 999999' },
   { ru:"Иордания",lt:"Jordanija",tr:"Ürdün", en: 'Jordan',flag: '🇯🇴',code: 'JO', dialCode: '+962', mask: '99 9999 9999' },
   { ru:"Казахстан",lt:"Kazachstanas",tr:"Kazakistan", en: 'Kazakhstan',flag: '🇰🇿',code: 'KZ', dialCode: '+7', mask: '9 (999) 999 9999' },
-  { ru:"Кения",lt:"Kenija",tr:"Kenya", en: 'Kenya',flag: '🇰🇪',code: 'KE', dialCode: '+254', mask: '9999 999999' },
+  { ru:"Кения",lt:"Kenija",tr:"Kenya", en: 'Kenya',flag: '🇰🇪',code: 'KE', dialCode: '+254', mask: '999 999999' },
   { ru:"Кирибати",lt:"Kiribatis",tr:"Kiribati", en: 'Kiribati',flag: '🇰🇮',code: 'KI', dialCode: '+686', mask: '99999999' },
   { ru:"Корея, Корейская Народно-Демократическая Республика",lt:"Korėja, Korėjos Liaudies Demokratinė Respublika",tr:"Kore, Kore Demokratik Halk Cumhuriyeti", en: "Korea, Democratic People's Republic of Korea",flag: '🇰🇵',code: 'KP', dialCode: '+850', mask: '(9) 99999' },
   { ru:"Корея, Республика Южная Корея",lt:"Korėja, Pietų Korėjos Respublika",tr:"Güney Kore Cumhuriyeti", en: 'Korea, Republic of South Korea',flag: '🇰🇷',code: 'KR', dialCode: '+82', mask: '999-9999-9999' },
